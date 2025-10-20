0270a6e2a8d005a4884633b7c27fb9655122eb58
Brayan Daniel Bernal Lopez <bblopezbernal123@gmail.com>
2025-10-20 18:40:59 -0500

    Actualizar componentes de Angular para compatibilidad con la nueva versión

--- frontend/src/app/components/calendar/calendar.ts
+++ frontend/src/app/components/calendar/calendar.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-calendar',
+  standalone: true,
   imports: [],
   templateUrl: './calendar.html',
-  styleUrl: './calendar.css'
+  styleUrls: ['./calendar.css']
 })
 export class Calendar {
 
